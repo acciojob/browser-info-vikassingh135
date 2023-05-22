4b0a81296fbf47dce89db047b4d632da50429c73
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-23 00:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //your JS code here. If required.
-
 let div_ = document.getElementById("browser-info");
-div_.innerText = `You are using ${UserAgent.Browser} version ${UserAgent.BrowserVersion}`;
-
+// let browser = navigator["appName"];
+// alert(browser);
+div_.innerText = "You are using "+navigator["appName"]+" version "+navigator["appVersion"];
+// div_.innerText = "You are using "+browser;
 
